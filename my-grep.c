4b1cdd1e37556188fa8c378f3eab849de9e23b55
Juho <37773658+JuhoKon@users.noreply.github.com>
2018-12-10 21:27:39 +0200

    Create my-grep.c

---
+++ my-grep.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  char c[150];
+
+  if (argc <= 1) {
+    printf("my-grep: searchterm [file...]\n");
+    exit(1);
+  }
+  if (argc == 2) {
+    while(fgets(c,149,stdin)!=NULL) {
+      if (strstr(c,argv[1])) {	
+	printf("%s",c);
+      }
+    }
+    return 0;
+  }
+  
+  FILE *fp = fopen(argv[2], "r"); 
+  if (fp == NULL) { 
+   printf("my-grep: cannot open file\n");
+   exit(1);
+  }
+  while(fgets(c,149,fp)!=NULL) {
+    if (strstr(c,argv[1])) {
+    printf("%s",c);
+    }
+  }
+  fclose(fp);
+  return 0;
+}
