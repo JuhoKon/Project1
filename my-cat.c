91faac6936a6b37724d9674515bb3fb368e169f9
Juho <37773658+JuhoKon@users.noreply.github.com>
2018-12-10 22:36:21 +0200

    Update my-cat.c

--- my-cat.c
+++ my-cat.c
@@ -1,6 +1,6 @@
 /* my-cat -ohjelma */
 /* Tekijät: Juho Kontiainen 0503209 */
-/*          Jesse Peltola   0523140 */
+/*          Jesse Peltola   0523140  */
 /* Lähteet: stackoverflow           */
 
 #include <stdio.h>
@@ -8,6 +8,9 @@
  
 int main(int argc, char *argv[]) {
   char c;
+  if (argc <= 1) { /*Ei argumentteja */
+    return 0;
+  }
   FILE *fp = fopen(argv[1], "r"); 
   if (fp == NULL) { 
    printf("cannot open file\n");
