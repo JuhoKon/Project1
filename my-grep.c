c87b3b7eeef5648b43171c189808a435f9d5ff23
Juho <37773658+JuhoKon@users.noreply.github.com>
2018-12-10 22:16:17 +0200

    Update my-grep.c

--- my-grep.c
+++ my-grep.c
@@ -1,6 +1,6 @@
 /* my-grep -ohjelma */
 /* Tekijät: Juho Kontiainen 0503209 */
-/*          Jesse Peltola           */
+/*          Jesse Peltola   0523140 */
 /* Lähteet: stackoverflow           */
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,20 +9,20 @@
 int main(int argc, char *argv[]) {
   char c[150];
 
-  if (argc <= 1) {
+  if (argc <= 1) { /*grep vaatii jotain argumentteja */
     printf("my-grep: searchterm [file...]\n");
     exit(1);
   }
-  if (argc == 2) {
+  if (argc == 2) { /*jos 2 argumenttia,halutaan lukea terminaalia */
     while(fgets(c,149,stdin)!=NULL) {
-      if (strstr(c,argv[1])) {	
-	printf("%s",c);
+      if (strstr(c,argv[1])) {	/*strstr-funktiolla verrataan merkkijonoja keskenään ja printataan kyseinen merkkijono uudestaan jos vastaava löytyy */
+	printf("%s",c);								
       }
     }
     return 0;
   }
   
-  FILE *fp = fopen(argv[2], "r"); 
+  FILE *fp = fopen(argv[2], "r"); /*muutoin luetaan tiedostosta */
   if (fp == NULL) { 
    printf("my-grep: cannot open file\n");
    exit(1);
