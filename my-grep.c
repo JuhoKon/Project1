ef1e1d936daee0f47e8a34bf2bb1beb06dd2c70b
Juho <37773658+JuhoKon@users.noreply.github.com>
2018-12-10 23:01:19 +0200

    Update my-grep.c

--- my-grep.c
+++ my-grep.c
@@ -1,19 +1,22 @@
+/* my-grep -ohjelma */
 /* Tekijät: Juho Kontiainen 0503209 */
 /*          Jesse Peltola   0523140 */
 /* Lähteet: stackoverflow           */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAXBUFFER 1024
+int grep(char*,char *);
 
 int main(int argc, char *argv[]) {
-  char c[480];
-
+  char c[MAXBUFFER];
+  int i;
   if (argc <= 1) { /*grep vaatii jotain argumentteja */
     printf("my-grep: searchterm [file...]\n");
     exit(1);
   }
   if (argc == 2) { /*jos 2 argumenttia,halutaan lukea terminaalia */
-    while(fgets(c,479,stdin)!=NULL) { /*lukee rivit, jotka ovat alle 480 kokoisia */ 
+    while(fgets(c,MAXBUFFER-1,stdin)!=NULL) { /*lukee rivit, jotka ovat alle 480 kokoisia */ 
       if (strstr(c,argv[1])) {	/*strstr-funktiolla verrataan merkkijonoja keskenään ja printataan kyseinen merkkijono uudestaan jos vastaava löytyy */
 	printf("%s",c);								
       }
@@ -21,13 +24,22 @@ int main(int argc, char *argv[]) {
     return 0;
   }
   
-  FILE *fp = fopen(argv[2], "r"); /*muutoin luetaan tiedostosta */
+  for (i=2;i<argc;++i) { /*käydään parametrit läpi */
+    printf("%s\n",argv[i]);
+    grep(argv[1],argv[i]); /*annetaan grep-funktiolle tiedostonimi sekä etsittävä avainsana */
+  }
+  return 0;
+}
+int grep(char *key,char *argv) {
+  char c[MAXBUFFER];
+  FILE *fp = fopen(argv, "r"); /*muutoin luetaan tiedostosta */
   if (fp == NULL) { 
    printf("my-grep: cannot open file\n");
    exit(1);
   }
-  while(fgets(c,479,fp)!=NULL) { /*lukee rivit, jotka ovat alle 480 kokoisia */ 
-    if (strstr(c,argv[1])) {
+  while(fgets(c,MAXBUFFER,fp)!=NULL) { /*lukee rivit, jotka ovat alle 480 kokoisia */
+    printf("%s",c);
+    if (strstr(c,key)) {
     printf("%s",c);
     }
   }
